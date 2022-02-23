5bffd2adfb2adf0c3713bdef623ba13439be61d2
akchindo <abdullahichindo16@gmail.com>
2022-02-23 20:43:12 +0100

    completing projects

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import location from './components/images/location.png'
 import Header from './components/Header/Header';
 import Journal from './components/Journal/Journal';
+import data from './data';
+
+console.log(data)
+const journals = data.map((journal) => {
+  return <Journal 
+      key={journal.id}
+      {...journal}
+  />
+})
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Journal />
+      {journals}
+      {/* <Journal /> */}
       {/* <Journal /> */}
 
     
